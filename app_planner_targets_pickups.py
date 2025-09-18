ddecad2bc96b5a6327665ad742a02ed74da485bb
howanitzeric-cmd <howanitzeric@gmail.com>
2025-09-18 20:24:53 +0200

    Add files via upload

---
+++ app_planner_targets_pickups.py
@@ -0,0 +1,8 @@
+# app_planner_targets_pickups.py
+import streamlit as st
+import pandas as pd
+import re, math
+from io import BytesIO
+
+# ... gesamter oben erstellter Code ...
+st.caption("Artefact Planner ready.")
